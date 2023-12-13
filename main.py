d6376a52d8ef401418714626631cc5d635438943
Bruno <brunopirizoliveira@gmail.com>
2023-12-13 16:53:52 -0300

    Ajuste na main

--- main.py
+++ main.py
@@ -114,10 +114,10 @@ def waitToDownload(data_hora_inicial) :
 
 
                     waitToDownload(data_hora_inicial)
-        else :
-            exit()
+        # else :
+        #     exit()
 
-data_hora_inicial = datetime.now()
+
 
 domain = config('DOMAIN')
 cpf = config('CPF')
@@ -125,112 +125,119 @@ user = config('USER')
 passw = config('PASSW')
 url = config('URL')
 
+filiais = ['MTZ']
 
-chrome_options = Options()
-chrome_options.add_argument("--headless") 
-driver = webdriver.Chrome()
-driver.get(url)
-driver.implicitly_wait(0.5)
+for filial in filiais:
+    data_hora_inicial = datetime.now()
 
-domainInput = driver.find_element(By.ID, "1")
-cpfInput = driver.find_element(By.ID, "2")
-userInput = driver.find_element(By.ID, "3")
-passInput = driver.find_element(By.ID, "4")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") 
+    driver = webdriver.Chrome()
+    driver.get(url)
+    driver.implicitly_wait(0.5)
 
-
-domainInput.send_keys(domain)
-cpfInput.send_keys(cpf)
-userInput.send_keys(user)
-passInput.send_keys(passw)
-
-submit_button = driver.find_element(By.ID, "5")
-submit_button.click()
-
-# html = urlopen(url)
-# bs = BeautifulSoup(html, 'html.parser')
-
-# domainInput = bs.find('input', {'id':'1'})
-# cpfInput    = bs.find('input', {'id':'2'})
-# userInput   = bs.find('input', {'id':'3'})
-# passInput   = bs.find('input', {'id':'4'})
-time.sleep(2)
-
-# Aqui estou selecionando a filial
-filial='LAJ'
-filialInput = driver.find_element(By.NAME, "f2")
-filialInput.send_keys(Keys.BACKSPACE)
-filialInput.send_keys(Keys.BACKSPACE)
-filialInput.send_keys(Keys.BACKSPACE)
-filialInput.send_keys(Keys.BACKSPACE)
-time.sleep(2)
-filialInput.send_keys(filial)
-time.sleep(2)
-
-# Aqui estou selecionando o menu na opcao
-opcao=455
-opcaoInput = driver.find_element(By.NAME, "f3")
-opcaoInput.send_keys(455)
-opcaoInput.send_keys(Keys.ENTER)
+    domainInput = driver.find_element(By.ID, "1")
+    cpfInput = driver.find_element(By.ID, "2")
+    userInput = driver.find_element(By.ID, "3")
+    passInput = driver.find_element(By.ID, "4")
 
 
-# Aqui ja estou na pagina de menus
-urlMenu = config('URL_MENU')
-driver.get(urlMenu)
+    domainInput.send_keys(domain)
+    cpfInput.send_keys(cpf)
+    userInput.send_keys(user)
+    passInput.send_keys(passw)
 
-time.sleep(5)
-# print(driver.page_source.encode('utf-8'))
+    submit_button = driver.find_element(By.ID, "5")
+    submit_button.click()
 
-#id 35 = E
-#id 37 = H
-#ID 11 = 011023
-unidadeInput = driver.find_element(By.ID, "2")
-unidadeInput.send_keys(Keys.BACKSPACE)
-unidadeInput.send_keys(Keys.BACKSPACE)
-unidadeInput.send_keys(Keys.BACKSPACE)
-time.sleep(2)
-unidadeInput.send_keys(filial)
-time.sleep(2)
+    # html = urlopen(url)
+    # bs = BeautifulSoup(html, 'html.parser')
 
-dtIniInput = driver.find_element(By.ID, "11")
-dtIniInput.clear()
-time.sleep(2)
-dtIniInput.send_keys("011023")
+    # domainInput = bs.find('input', {'id':'1'})
+    # cpfInput    = bs.find('input', {'id':'2'})
+    # userInput   = bs.find('input', {'id':'3'})
+    # passInput   = bs.find('input', {'id':'4'})
+    time.sleep(2)
 
-arquivoInput = driver.find_element(By.ID, "35")
-arquivoInput.clear()
-time.sleep(2)
-arquivoInput.send_keys("E")
-
-complInput = driver.find_element(By.ID, "37")
-complInput.clear()
-time.sleep(2)
-complInput.send_keys(Keys.BACKSPACE)
-time.sleep(2)
-complInput.send_keys("H")
-
-submit_menu_button = driver.find_element(By.ID, "40")
-time.sleep(2)
-submit_menu_button.click()
-
-time.sleep(5)
-
-# Aqui ja estou na pagina de relatorios
-
-# print(tableReports)
-
-# Inicialize uma variável para contar as células
-# Obtenha a data e hora atual
-
-waitToDownload(data_hora_inicial)
-#Configuracao Excel
-# configExcel = driver.find_element(By.ID, "36")
-# configExcel.click()
-# time.sleep(50)
-# print(driver.page_source.encode('utf-8'))
+    # Aqui estou selecionando a filial
 
 
 
-# # print(bs.prettify())
-# print(cpfInput)
-# print(userInput)
-# print(passInput)
+
+    filialInput = driver.find_element(By.NAME, "f2")
+    filialInput.send_keys(Keys.BACKSPACE)
+    filialInput.send_keys(Keys.BACKSPACE)
+    filialInput.send_keys(Keys.BACKSPACE)
+    filialInput.send_keys(Keys.BACKSPACE)
+    time.sleep(2)
+    filialInput.send_keys(filial)
+    time.sleep(2)
+
+    # Aqui estou selecionando o menu na opcao
+    opcao=455
+    opcaoInput = driver.find_element(By.NAME, "f3")
+    opcaoInput.send_keys(455)
+    opcaoInput.send_keys(Keys.ENTER)
+
+
+    # Aqui ja estou na pagina de menus
+    urlMenu = config('URL_MENU')
+    driver.get(urlMenu)
+
+    time.sleep(5)
+    # print(driver.page_source.encode('utf-8'))
+
+    #id 35 = E
+    #id 37 = H
+    #ID 11 = 011023
+    unidadeInput = driver.find_element(By.ID, "2")
+    unidadeInput.send_keys(Keys.BACKSPACE)
+    unidadeInput.send_keys(Keys.BACKSPACE)
+    unidadeInput.send_keys(Keys.BACKSPACE)
+    time.sleep(2)
+    # unidadeInput.send_keys(filial)
+    # time.sleep(2)
+
+    dtIniInput = driver.find_element(By.ID, "11")
+    dtIniInput.clear()
+    time.sleep(2)
+    dtIniInput.send_keys("011123")
+
+    arquivoInput = driver.find_element(By.ID, "35")
+    arquivoInput.clear()
+    time.sleep(2)
+    arquivoInput.send_keys("E")
+
+    complInput = driver.find_element(By.ID, "37")
+    complInput.clear()
+    time.sleep(2)
+    complInput.send_keys(Keys.BACKSPACE)
+    time.sleep(2)
+    complInput.send_keys("H")
+
+    submit_menu_button = driver.find_element(By.ID, "40")
+    time.sleep(2)
+    submit_menu_button.click()
+
+    time.sleep(5)
+
+    # Aqui ja estou na pagina de relatorios
+
+    # print(tableReports)
+
+    # Inicialize uma variável para contar as células
+    # Obtenha a data e hora atual
+
+    waitToDownload(data_hora_inicial)
+    #Configuracao Excel
+    # configExcel = driver.find_element(By.ID, "36")
+    # configExcel.click()
+    # time.sleep(50)
+    # print(driver.page_source.encode('utf-8'))
+
+
+
+    # # print(bs.prettify())
+    # print(cpfInput)
+    # print(userInput)
+    # print(passInput)
